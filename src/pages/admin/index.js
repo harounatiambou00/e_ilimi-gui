0908f6a34bd8246ca6673f333d0b58cf8ad4bcbc
Harouna Tiambou Abdoul Wahabou <harounatiambou.pro@gmail.com>
2022-04-04 01:06:39 +0100

    Files & Folders structure review to built the foundation (base) of the application

---
+++ src/pages/admin/index.js
@@ -0,0 +1,2 @@
+export {default as AdminHome} from './home/Home'
+export {default as AdminSignIn} from './sign-in/SignIn'
---
+++ src/pages/user/index.js
@@ -0,0 +1,3 @@
+export {default as UserHome} from './home/Home'
+export {default as UserSignIn} from './sign-in/SignIn'
+export {default as UserSignUp} from './sign-up/SignUp'
